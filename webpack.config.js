422a99f904ce1e4ee0bc845e0f2ba7016cca4705
paynedeng <payne.d1991@gmail.com>
2020-04-27 00:10:47 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
-var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var path = require('path')
 
 module.exports = {
     mode: 'development',
+    devtool: 'inline-source-map',
+    devServer: {
+        contentBase: './dist',
+    },
     entry: './src/index.js',
     output: {
-        filename: '[name].[contenthash].js'
-    }
-};
+        filename: 'index.[contenthash].js'
+    },
+    plugins: [new HtmlWebpackPlugin({
+        title: 'PAYNE - 佩恩',
+        template: 'src/assets/index.html'
+    })],
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+}
